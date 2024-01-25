b757ced4e62fe55e7835b4897f15d79b35ef0fdc
navruz22 <navruz.ochilov98@mail.ru>
2024-01-25 17:33:46 +0500

    done

--- script.js
+++ script.js
@@ -1,3 +1,43 @@
 var fullname = 'Jobir Shukrullayev';
 var x = 45;
-console.log(fullname);
+var isMarried = false;
+var res = x + fullname;
+// console.log(res);
+var num = null;
+var a = undefined;
+// console.log(num);
+// array
+var colors = ['red', 'green', 'blue'];
+var numbers = [2, 4, 8];
+// console.log(numbers);
+// any
+var smth = 1;
+// console.log(smth)
+smth = 'Hakimjon';
+// console.log(smth)
+smth = true;
+// console.log(smth)
+smth = ['black', 5, true];
+// console.log(smth)
+smth = {
+    name: 'Jobir',
+    birthYear: 1996,
+    job: 'programmer'
+};
+// console.log(smth)
+// Function
+function logger(a) {
+    if (typeof a === 'number')
+        console.log(a.toFixed());
+    else if (typeof a === 'string')
+        console.log(a.toLowerCase());
+}
+// logger(45)
+var nums = [1, 2, 3, 4, 5, 6];
+nums.map(function (num) { return num.toFixed(); });
+// producedYear - required
+// producedYear? - not required
+var logCar = function (car) {
+    return "Name of car ".concat(car.name, ", color ").concat(car.color, ", produced in ").concat(car.producedYear);
+};
+console.log(logCar({ name: 'BMW', color: 'black', producedYear: 2020 }));
--- script.ts
+++ script.ts
@@ -1,5 +1,55 @@
 const fullname: string = 'Jobir Shukrullayev'
-
 const x: number = 45
+const isMarried: boolean = false
+const res: string = x + fullname
+// console.log(res);
 
-console.log(fullname);
+
+const num: null = null
+const a: undefined = undefined
+// console.log(num);
+
+
+// array
+const colors: string[] = ['red', 'green', 'blue']
+const numbers: number[] = [2, 4, 8]
+// console.log(numbers);
+
+
+// any
+let smth: any = 1
+// console.log(smth)
+smth = 'Hakimjon'
+// console.log(smth)
+smth = true
+// console.log(smth)
+smth = ['black', 5, true]
+// console.log(smth)
+smth = {
+    name: 'Jobir',
+    birthYear: 1996,
+    job: 'programmer'
+}
+// console.log(smth)
+
+
+// Function
+
+function logger(a: number | string): void {
+    if (typeof a === 'number')
+        console.log(a.toFixed())
+    else if (typeof a === 'string')
+        console.log(a.toLowerCase())
+}
+// logger(45)
+
+
+const nums: number[] = [1, 2, 3, 4, 5, 6]
+nums.map((num: number) => num.toFixed())
+
+// producedYear - required
+// producedYear? - not required
+const logCar = (car: {name: string, color: string, producedYear?: number}) => {
+    return `Name of car ${car.name}, color ${car.color}, produced in ${car.producedYear}`
+}
+console.log(logCar({name: 'BMW', color: 'black', producedYear: 2020}));
